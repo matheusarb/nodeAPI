1f353ef39810b3e4dadcf9cc6f28892f378ae7ba
Matheus <mat.araujoribeiro@gmail.com>
2023-12-06 16:46:15 -0300

    [add]query params

--- src/server.js
+++ src/server.js
@@ -3,7 +3,24 @@ const app = express();
 const PORT = 3333;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
 
+//ROUTE PARAMS
+//são OBRIGATóRIOS no endereço da rota
 //1º argumento: rota/endereço; 2º argumento: função
-app.get(`/message/:id`, (req, resp) => {
-    resp.send(`Id da msg: ${req.params.id}`);
+app.get(`/message/:id/:user`, (request, response) => {
+    // destructuring
+    const { id, user } = request.params;
+    
+    //recuperar info que tá sendo passada como 'route params':
+    response.send(`
+        Msg ID: ${id}\n
+        Usuário: ${user}
+    `);
 });
+
+//QUERY PARAMS
+//São OPCIONAIS
+app.get(`/users`, (request, response) => {
+    const { pages, limit } = request.query;
+
+    response.send(`Página: ${pages}. Mostrar: ${limit}`)
+})
