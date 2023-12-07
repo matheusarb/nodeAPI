041bdb15d2c4b924f6fa6c3fdc4e9af2b2dd0e82
Matheus <mat.araujoribeiro@gmail.com>
2023-12-07 15:09:11 -0300

    [add] middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,8 +5,19 @@ const usersRoutes = Router(); //inicializando a função de rotear
 const UsersController = require("../controllers/UsersController"); //importando o Controller, onde há a BLL
 const usersController = new UsersController(); //criando instância da classe
 
+function myMiddleware(request, response, next) {
+    console.log("Você passou pelo middleware!");
+    if (!request.body.isAdmin ||  request.body.isAdmin == null) {
+        return response.json({ message: "User unauthorized." })
+    }
+    response.json({ message: "Authorization complete. User created." })
+    
+    next();
+}
+
+
 //POST e INSOMNIA
-usersRoutes.post('/', usersController.create);
+usersRoutes.post('/', myMiddleware, usersController.create);
 
 //exportar o usersRoutes ao server.js
 module.exports = usersRoutes;
