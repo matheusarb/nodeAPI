8ba533113672ff8dd2f61ac42225ac13b15caa4f
Matheus <mat.araujoribeiro@gmail.com>
2023-12-11 17:15:05 -0300

    [add] password update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,8 @@
-const { hash } = require("bcrypt");
+const { hash, compare } = require("bcrypt");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
-class UsersController {
+class UsersController {   
     async getUser(request, response) {
         const { id } = request.params;
         const database = await sqliteConnection();
@@ -42,7 +42,7 @@ class UsersController {
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
@@ -54,18 +54,32 @@ class UsersController {
         const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
         if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
             throw new AppError("Este e-mail já está em uso.");
-        }
-
+        }      
+        
         user.name = name;
         user.email = email;
+        
+        //Password exchange
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha atual para realizar a troca.");
+        }
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+            if(!checkOldPassword) {
+                throw new AppError("A senha atual está incorreta. Digite a senha novamente para prosseguir");
+            }
+
+            user.password = await hash(password, 8)
+        }
 
         await database.run(`
                 UPDATE users SET
                 name = ?,
                 email = ?,
+                password = ?,
                 updated_at = ?
                 WHERE id = ?`,
-                [user.name, user.email, new Date(), id]
+                [user.name, user.email, user.password, new Date(), id]
         );
 
         return response.json();
