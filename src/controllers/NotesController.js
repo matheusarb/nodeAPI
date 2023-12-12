8bd7ec36493a9da5a8aed5c6eb6ce524f7a0e1ab
Matheus <mat.araujoribeiro@gmail.com>
2023-12-12 16:33:33 -0300

    [add] NotesController.show orderering

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -34,9 +34,9 @@ class NotesController {
     async show(request, response) {
         const { id } = request.params;
 
-        const note = await knex("notes").where({ id: id }).first();
-        const tags = await knex("tags").where({ note_id: id });
-        const links = await knex("links").where({ note_id: id });
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at");
         
         return response.json({
             ...note,
