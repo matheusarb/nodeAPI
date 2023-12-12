9dc7dbbe9bc908669f1745b069b0328cb4be5717
Matheus <mat.araujoribeiro@gmail.com>
2023-12-12 10:46:25 -0300

    [add] NotesController

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,25 @@
+const knex = require("../database/knex");
+
+class NotesController {
+    async create(request, response) {
+        const { title, description, tags, links } = request.body;
+        const { user_id } = request.params;
+
+        const note_id = await knex("notes").insert({
+            title,
+            description,
+            user_id
+        });
+
+        const linksInsert = links.map(link => {
+            return {
+                note_id,
+                url: link
+            }
+        });
+        await knex("links").insert(linksInsert); 
+
+    }
+}
+
+module.exports = NotesController;
