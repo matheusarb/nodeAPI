52eab7c8812efad85c8360d3e7a904dbd7c97e14
Matheus <mat.araujoribeiro@gmail.com>
2023-12-14 10:54:51 -0300

    [add] map TagsController response

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -5,8 +5,14 @@ class TagsController {
         const { id } = request.params;
 
         const userTags = await knex("tags").where({ user_id: id });
+        const tagsProps = userTags.map(tag => {
+            return {
+                name: tag.name,
+                note_id: tag.note_id
+            }
+        })
 
-        return response.json(userTags);
+        return response.json(tagsProps);
     }
 }
 
